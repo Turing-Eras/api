4188e4f855e0db1ea7ac1728bb322e6cb74b2c41
Angela Guardia <angelaguardia20@gmail.com>
2020-12-19 15:15:15 -0800

    Added model validations, relations and tests - for users, eras, events
    Co-Authored-By: Luke Hunter James-Erickson <theeluke@gmail.com>
    Co-Authored-By: Leah Riffell <leahriffell@users.noreply.github.com>
    Co-Authored-By: Daniel Halverson <63027888+dhalverson@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :birthdate, presence: true
+
+  has_many :eras
+  has_many :events
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :birthdate }
+  end
+
+  describe 'relationships' do
+    it { should have_many :eras }
+    it { should have_many :events }
+  end
 end
