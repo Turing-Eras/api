20f5a80c98774c816e96df5aaede9959b7d1421d
Leah Riffell <leah.riffell@gmail.com>
2020-12-29 16:00:16 -0700

    Add era request to user query and mutation tests

--- spec/requests/graphql/queries/users/get_single_user_spec.rb
+++ spec/requests/graphql/queries/users/get_single_user_spec.rb
@@ -9,12 +9,27 @@ RSpec.describe Types::QueryType do
       result = JSON.parse(response.body)
 
       expect(result.dig('data',
-                        'getUser')).to eq({
-                                            'id' => user.id.to_s,
-                                            'name' => user.name,
-                                            'email' => user.email,
-                                            'birthdate' => user.birthdate.to_s
-                                          })
+                        'getUser')).to include({
+                                                 'id' => user.id.to_s,
+                                                 'name' => user.name,
+                                                 'email' => user.email,
+                                                 'birthdate' => user.birthdate.to_s
+                                               })
+
+      expect(result['data']['getUser']['eras']).to eq(
+        user.eras.map do |era|
+          {
+            'id' => era.id.to_s,
+            'userId' => era.user_id.to_s,
+            'name' => era.name.to_s,
+            'startDate' => era.start_date.to_s,
+            'endDate' => era.end_date.to_s,
+            'color' => era.color,
+            'createdAt' => era.created_at.to_s,
+            'updatedAt' => era.updated_at.to_s
+          }
+        end
+      )
     end
   end
 
@@ -26,6 +41,16 @@ RSpec.describe Types::QueryType do
           name
           email
           birthdate
+          eras {
+            id
+            userId
+            name
+            startDate
+            endDate
+            color
+            createdAt
+            updatedAt
+          }
         }
       }
     GQL
--- spec/requests/graphql/queries/users/get_users_spec.rb
+++ spec/requests/graphql/queries/users/get_users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Types::QueryType do
   describe 'display users' do
     it 'can query all users' do
-      create_list(:user, 2)
+      create_list(:user, 2, :with_eras_events)
 
       post graphql_path, params: { query: query }
       result = JSON.parse(response.body)
@@ -11,9 +11,10 @@ RSpec.describe Types::QueryType do
       expect(result['data']['getUsers'].count).to eq(2)
 
       users = User.all
+
       expect(result.dig('data', 'getUsers')).to match_array(
         users.map do |user|
-          { 'id' => user.id.to_s, 'name' => user.name, 'email' => user.email, 'birthdate' => user.birthdate.to_s }
+          { 'id' => user.id.to_s, 'name' => user.name, 'email' => user.email, 'birthdate' => user.birthdate.to_s, 'eras' => user.eras.map {|era| { 'id' => era.id.to_s }}}
         end
       )
     end
@@ -27,7 +28,10 @@ RSpec.describe Types::QueryType do
           name
           email
           birthdate
+          eras {
+            id
           }
+        }
       }
     GQL
   end
