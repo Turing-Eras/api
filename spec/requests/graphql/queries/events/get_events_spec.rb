912392a57c66a457aea1ed0d90f01da327090a84
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-12-31 14:48:03 -0700

    Adds get events testing

--- spec/requests/graphql/queries/events/get_events_spec.rb
+++ spec/requests/graphql/queries/events/get_events_spec.rb
@@ -8,16 +8,26 @@ RSpec.describe Types::QueryType do
       post graphql_path, params: { query: query }
       result = JSON.parse(response.body)
 
+      # I think this test may be able to be moved into Leah's user spec, as I've just added Events to be queried on a user.
     end
   end
 
   def query
     <<~GQL
       {
-        getEvents {
-          
+        getUsers {
+          id
+          name
+          email
+          birthdate
+          events {
+            id
+            name
+            date
+            color
+          }
         }
       }
-
+    GQL
   end
 end
