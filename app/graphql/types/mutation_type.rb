ab17642c25135eb99ea22db71beac671fd9a6f0c
LHJE <theeluke@gmail.com>
2021-01-04 15:57:28 -0700

    Add update_event to mutations_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :update_user, mutation: Mutations::Users::UpdateUser
+    field :update_event, mutation: Mutations::Events::UpdateEvent
     field :delete_era, mutation: Mutations::Eras::DeleteEra
   end
 end
