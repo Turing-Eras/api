cd6a6d914b949596f03cc3eb139ff85dc7697ada
Angela Guardia <angelaguardia20@gmail.com>
2021-01-04 12:52:44 -0800

    Update events factory to give random event date

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event do
     name { Faker::Hipster.sentence }
-    date { '2020-12-19 15:38:49' }
+    date { Faker::Date.between(from: 10.years.ago, to: Date.today) }
     color { Faker::Color.hex_color }
     association :user
   end
