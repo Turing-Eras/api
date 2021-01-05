de6dae92e97099100925356fc56382712babf248
Angela Guardia <angelaguardia20@gmail.com>
2021-01-05 11:24:39 -0800

    Add createEra to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :update_user, mutation: Mutations::Users::UpdateUser
     field :delete_era, mutation: Mutations::Eras::DeleteEra
+    field :create_era, mutation: Mutations::Eras::CreateEra
   end
 end
