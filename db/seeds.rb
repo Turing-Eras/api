7d7828fcba18cf87d95f6a9f3891f32eee01b980
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-05 15:05:16 -0700

    Co-Authored by:

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,8 @@ Era.destroy_all
 Era.reset_pk_sequence
 User.destroy_all
 User.reset_pk_sequence
-
-Event.delete_all
-Era.delete_all
-User.delete_all
+Question.destroy_all
+Question.reset_pk_sequence
 
 frosty = User.create!(name: 'Frosty', email: 'snow@brr.com', birthdate: Date.new(2001,2,3))
 santa = User.create!(name: 'Nick', email: 'slay@northpole.com', birthdate: Date.new(1950,12,25))
@@ -27,3 +25,11 @@ FactoryBot.create_list(:era, 3, user: santa)
 
 FactoryBot.create_list(:event, 3, user: frosty)
 FactoryBot.create_list(:event, 3, user: santa)
+
+Question.create!(name: 'Birthdate', question: 'When is your birthday?', onboarding: true)
+Question.create!(name: 'Graducated from high school', question: 'When did you graduate high school?', onboarding: true)
+Question.create!(name: 'Graduated from college', question: 'When did you graduate college?', onboarding: true)
+Question.create!(name: 'Moved to current house', question: 'What is your birthday?', onboarding: true)
+Question.create!(name: 'Got married', question: "If you're currently married, when were you married?", onboarding: true)
+Question.create!(name: 'First job', question: 'What are the approximate dates that you started and ended your first job?', onboarding: true)
+Question.create!(name: 'Best vacation', question: 'What are the dates of your most memorable vacation?', onboarding: true)
