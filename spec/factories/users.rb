de7d209150d611ee1c6f58c853c0616122a49392
LHJE <theeluke@gmail.com>
2021-01-05 16:08:40 -0700

    Update user factory birthdate to further in the past

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.unique.email }
-    birthdate { '2020-12-19 15:34:36' }
+    birthdate { '1991-12-19 15:34:36' }
 
     trait :with_eras_events do
       after(:create) do |user|
