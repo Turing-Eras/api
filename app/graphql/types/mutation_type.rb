a96a51ac89b0742bf84405d5748c29e80afd7d31
Leah Riffell <leah.riffell@gmail.com>
2021-01-04 20:23:14 -0700

    Add update era mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :update_user, mutation: Mutations::Users::UpdateUser
+
+    field :update_era, mutation: Mutations::Eras::UpdateEra
   end
 end
