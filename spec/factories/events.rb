17680788d7314550c72315605a0b491da9431ea7
LHJE <theeluke@gmail.com>
2021-01-05 16:08:18 -0700

    Updates events factory with week_number

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :event do
     name { Faker::Hipster.sentence }
     date { Faker::Date.between(from: 10.years.ago, to: Date.today) }
+    week_number { 22 }
     color { Faker::Color.hex_color }
     association :user
   end
