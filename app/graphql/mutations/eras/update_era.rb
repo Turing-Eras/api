f09c772e1cad243853b688655f3d2d845e53de86
LHJE <theeluke@gmail.com>
2021-01-06 12:08:25 -0700

    Update update_era to have start_ and end_week, not week_number

--- app/graphql/mutations/eras/update_era.rb
+++ app/graphql/mutations/eras/update_era.rb
@@ -3,7 +3,8 @@ class Mutations::Eras::UpdateEra < ::Mutations::BaseMutation
   argument :name, String, required: false
   argument :start_date, String, required: false
   argument :end_date, String, required: false
-  argument :week_number, String, required: false
+  argument :start_week, String, required: false
+  argument :end_week, String, required: false
   argument :color, String, required: false
 
   type Types::EraType
@@ -11,8 +12,10 @@ class Mutations::Eras::UpdateEra < ::Mutations::BaseMutation
   def resolve(attributes)
     era = Era.find(attributes[:id])
     start_date = attributes[:start_date].to_date
+    end_date = attributes[:end_date].to_date
     user_bday = User.where(id: era[:user_id])[0].birthdate
-    attributes[:week_number] = ((start_date - user_bday).to_i / 7)
+    attributes[:start_week] = ((start_date - user_bday).to_i / 7)
+    attributes[:end_week] = ((end_date - user_bday).to_i / 7)
     era.update(attributes)
     era
   end
