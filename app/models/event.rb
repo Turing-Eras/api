15820899008be999b6268e74dde7970fbae40a60
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-06 14:30:10 -0700

    Adds color validation to event
    Co-Authored By:

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
   validates :week_number, presence: true
+  validates :color, presence: true
   belongs_to :user
 end
