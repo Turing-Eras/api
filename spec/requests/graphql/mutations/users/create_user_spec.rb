db27a4cab516662f27cba5908e1a492d94ebb710
Leah Riffell <leah.riffell@gmail.com>
2021-01-07 08:39:37 -0700

    Add sad path test for new user with existing email

--- spec/requests/graphql/mutations/users/create_user_spec.rb
+++ spec/requests/graphql/mutations/users/create_user_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe Mutations::Users::CreateUser, type: :request do
 
       expect(User.count).to eq(1)
     end
+
+    it 'cannot create a new user with an email that already exists' do
+      create(:user, email: 'user@email.com')
+      post graphql_path, params: { query: query(name: 'Assassin gallant', email: 'user@email.com', birthdate: '1990-10-10') }
+      result = JSON.parse(response.body)
+      
+      expect(result['errors'][0]['message']).to eq('Validation failed: Email has already been taken')
+    end
   end
 
   def query(name:, email:, birthdate:)
