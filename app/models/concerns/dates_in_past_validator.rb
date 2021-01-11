4530975469be85fb38572f9342511af6aed04c68
LHJE <theeluke@gmail.com>
2021-01-11 10:57:27 -0700

    Further update dates_in_past_validator to dry up code.

--- app/models/concerns/dates_in_past_validator.rb
+++ app/models/concerns/dates_in_past_validator.rb
@@ -1,10 +1,18 @@
 class DatesInPastValidator < ActiveModel::Validator
   def validate(record)
     if record.instance_of?(Era) && record.start_date && record.end_date
-      record.errors.add(:start_date, 'cannot be in the future') if record.start_date > Time.zone.today
-      record.errors.add(:end_date, 'cannot be in the future') if record.end_date > Time.zone.today
+      era(record)
     elsif record.instance_of?(Event) && record.date
-      record.errors.add(:date, 'cannot be in the future') if record.date > Time.zone.today
+      event(record)
     end
+  end
+
+  def era(record)
+    record.errors.add(:start_date, 'cannot be in the future') if record.start_date > Time.zone.today
+    record.errors.add(:end_date, 'cannot be in the future') if record.end_date > Time.zone.today
+  end
+
+  def event(record)
+    record.errors.add(:date, 'cannot be in the future') if record.date > Time.zone.today
   end
 end
