f3e312526e8d75396d7e491e20ed03742ae4c11c
LHJE <theeluke@gmail.com>
2021-01-11 10:53:07 -0700

    Update dates_in_past_validator.rb to appease rubocop errors

--- app/models/concerns/dates_in_past_validator.rb
+++ app/models/concerns/dates_in_past_validator.rb
@@ -1,10 +1,10 @@
 class DatesInPastValidator < ActiveModel::Validator
   def validate(record)
     if record.instance_of?(Era) && record.start_date && record.end_date
-      record.errors.add(:start_date, 'cannot be in the future') if record.start_date > Date.today
-      record.errors.add(:end_date, 'cannot be in the future') if record.end_date > Date.today
+      record.errors.add(:start_date, 'cannot be in the future') if record.start_date > Time.zone.today
+      record.errors.add(:end_date, 'cannot be in the future') if record.end_date > Time.zone.today
     elsif record.instance_of?(Event) && record.date
-      record.errors.add(:date, 'cannot be in the future') if record.date > Date.today
+      record.errors.add(:date, 'cannot be in the future') if record.date > Time.zone.today
     end
   end
 end
