eba9a319ed6f6337174a939c757fe74b9c233ed3
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-13 14:32:37 -0700

    Fix rubocop violation incorrect fix in helper methods

--- app/graphql/mutations/helper_methods.rb
+++ app/graphql/mutations/helper_methods.rb
@@ -4,7 +4,6 @@ module Mutations::HelperMethods
   end
 
   def self.set_color
-    ['#F7A83E', '#A94460', '#DB4709', '#96B40D', '#E7C408', '#70D6FF', '#FF70A6', '#AA9770', '#FFD670',
-     '#E9FF70'].sample
+    ['#F7A83E', '#A94460', '#DB4709', '#96B40D', '#E7C408', '#70D6FF', '#FF70A6', '#AA9770', '#FFD670', '#E9FF70'].sample
   end
 end
