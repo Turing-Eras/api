d6f17be5382a1d73968a672572d7fde0e30ddad9
Angela Guardia <angelaguardia20@gmail.com>
2021-01-14 08:06:32 -0800

    Add current_user field to user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,6 +4,7 @@ module Types
     field :name, String, null: true
     field :email, String, null: true
     field :birthdate, String, null: true
+    field :current_week, Integer, null: true
     field :events, [Types::EventType], null: false
     field :eras, [Types::EraType], null: false
   end
